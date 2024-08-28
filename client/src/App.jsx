a66203395fefe90aadfd20bcd541dd8f59cbce93
Abhinav Panwar <panwarabhinav35@gmail.com>
2024-08-28 23:53:34 +0530

    pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 const App = () => {
   return (
-    <div className='text-red-700'>App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/sign-in" element={<Signin></Signin>}></Route>
+        <Route path="/sign-up" element={<SignUp></SignUp>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/profile" element={<Profile></Profile>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
