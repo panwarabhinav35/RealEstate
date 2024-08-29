b3d08a917b0a640a1c1a39eab6659cbad57bfdfa
Abhinav Panwar <panwarabhinav35@gmail.com>
2024-08-29 16:11:35 +0530

    routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,11 @@ import Signin from "./pages/Signin";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 const App = () => {
   return (
     <BrowserRouter>
+      <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/sign-in" element={<Signin></Signin>}></Route>
